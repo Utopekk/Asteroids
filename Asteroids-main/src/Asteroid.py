5578910f93f753586fd35b3c70bb5e8a68e6891e
Eryk Nejfert <110306022+ErykNe@users.noreply.github.com>
2024-01-18 22:56:31 +0100

    Delete Asteroids-main directory

--- Asteroids-main/src/Asteroid.py
+++
@@ -1,7 +0,0 @@
-from SpaceObject import SpaceObject
-
-
-class Asteroid(SpaceObject):
-    def __init__(self, n_size, x, y, dx, dy, angle, vertices):
-        super().__init__(n_size, x, y, dx, dy, angle)
-        self.vertices = vertices
--- Asteroids-main/src/Bullet.py
+++
@@ -1,7 +0,0 @@
-from SpaceObject import SpaceObject
-
-
-class Bullet(SpaceObject):
-    def __init__(self, n_size, x, y, dx, dy, angle, acceleration):
-        super().__init__(n_size, x, y, dx, dy, angle)
-        self.acceleration = acceleration
